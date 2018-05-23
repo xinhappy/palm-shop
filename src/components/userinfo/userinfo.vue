659e71d282e55815dd3a6b17414bad5717ade22a
辛志周 <18768819362@163.com>
2018-05-23 15:49:31 +0800

    New changelist

---
+++ src/components/userinfo/userinfo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <x-header style="background-color:#fff" :left-options="{backText: '',showBack: false}">
+      <a slot="left" class="back" @click="back()"></a>
+      <a style="color: #333">用户信息</a>
+    </x-header>
+    <group title="常见故障" title-color="#333">
+      <cell title="连接路由器,宽带无法上网解决办法" is-link v-for="item in  [0, 1, 2, 3, 4]"></cell>
+    </group>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import {XHeader, Cell, Group} from 'vux'
+  export default {
+    components: {
+      XHeader,
+      Cell,
+      Group
+    },
+    methods: {
+      back (){
+        this.$router.replace('index')
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less" rel="stylesheet/less">
+  .back {
+    background: url(./images/back_bule.png) no-repeat;
+    background-size: contain;
+    display: inline-block;
+    width: 5vw;
+    height: 5vw;
+  }
+  .vux-header .vux-header-title {
+    color: #999;
+  }
+</style>
